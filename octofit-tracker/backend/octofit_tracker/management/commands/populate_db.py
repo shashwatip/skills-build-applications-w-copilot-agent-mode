97084c3e43e38d9ca8e1e35217621a0b58c90bb0
shashwatip <61790198+shashwatip@users.noreply.github.com>
2025-05-22 02:47:16 +0000

    commit step 3

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,94 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
+from datetime import timedelta
+
+class Command(BaseCommand):
+    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create users
+        users = [
+            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
+            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
+            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
+        ]
+
+        # Debugging: Print user data before saving
+        print("Creating users...")
+        for user in users:
+            print(user.username, user.email)
+
+        User.objects.bulk_create(users)
+
+        # Create teams
+        team1 = Team(_id=ObjectId(), name='Blue Team')
+        team2 = Team(_id=ObjectId(), name='Gold Team')
+        team1.save()
+        team2.save()
+        team1.members.add(users[0], users[1])
+        team2.members.add(users[2], users[3], users[4])
+
+        # Debugging: Print team data before saving
+        print("Creating teams...")
+        print("Team 1 members:", [user.username for user in team1.members.all()])
+        print("Team 2 members:", [user.username for user in team2.members.all()])
+
+        # Create activities
+        activities = [
+            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=timedelta(hours=1)),
+            Activity(_id=ObjectId(), user=users[1], activity_type='Crossfit', duration=timedelta(hours=2)),
+            Activity(_id=ObjectId(), user=users[2], activity_type='Running', duration=timedelta(hours=1, minutes=30)),
+            Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
+            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
+        ]
+
+        # Debugging: Print activity data before saving
+        print("Creating activities...")
+        for activity in activities:
+            print(activity.activity_type, activity.duration)
+
+        Activity.objects.bulk_create(activities)
+
+        # Create leaderboard entries
+        leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=users[0], score=100),
+            Leaderboard(_id=ObjectId(), user=users[1], score=90),
+            Leaderboard(_id=ObjectId(), user=users[2], score=95),
+            Leaderboard(_id=ObjectId(), user=users[3], score=85),
+            Leaderboard(_id=ObjectId(), user=users[4], score=80),
+        ]
+
+        # Debugging: Print leaderboard data before saving
+        print("Creating leaderboard entries...")
+        for entry in leaderboard_entries:
+            print(entry.user.username, entry.score)
+
+        Leaderboard.objects.bulk_create(leaderboard_entries)
+
+        # Create workouts
+        workouts = [
+            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
+            Workout(_id=ObjectId(), name='Crossfit', description='Training for a crossfit competition'),
+            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
+            Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
+            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
+        ]
+
+        # Debugging: Print workout data before saving
+        print("Creating workouts...")
+        for workout in workouts:
+            print(workout.name, workout.description)
+
+        Workout.objects.bulk_create(workouts)
+
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
